334fe688fe090ea18fb1114e6c6c6cb40920e2e1
roothheo <onlygogot@gmail.com>
2024-06-15 07:36:13 +0200

    ajout d'une calculatrice bash

---
+++ calculatrice.sh
@@ -0,0 +1,6 @@
+read -p "Entrez le premier nombre : " num1
+read -p "Entrez le deuxième nombre : " num2
+echo "Addition: $(($num1 + $num2))"
+echo "Soustraction: $(($num1 - $num2))"
+echo "Multiplication: $(($num1 * $num2))"
+echo "Division: $(($num1 / $num2))" 
